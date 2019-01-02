0240a444667a08b77a208e204ff8608a00fb666f
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2019-01-02 23:23:40 +0700

    Add files via upload

--- OctaDist_V1.py
+++ OctaDist_V1.py
@@ -34,10 +34,23 @@ Personal website: https://sites.google.com/site/rangsiman1993
 """
 
 import numpy as np
-
+from math import *
 from tkinter import *
 from tkinter.filedialog import askopenfilename
 from tkinter.messagebox import showinfo
+import matplotlib.pyplot as plt
+from mpl_toolkits.mplot3d.axes3d import Axes3D
+
+
+def donothing():
+    filewin = Toplevel(root)
+    button = Button(filewin, text="Do nothing button")
+    button.pack()
+
+
+# global variables
+filename = ""
+filedata = ""
 
 
 def openfile():
@@ -55,10 +68,11 @@ def openfile():
         return 1
 
     """Open text file"""
-    filename = askopenfilename(initialdir="C:/Users/", title="Choose input file",
-                               filetypes=(("XYZ File", "*.xyz"),
-                                          ("Text File", "*.txt"),
-                                          ("All Files", "*.*")))
+    filename = askopenfilename(  # initialdir="C:/Users/",
+        title="Choose input file",
+        filetypes=(("XYZ File", "*.xyz"),
+                   ("Text File", "*.txt"),
+                   ("All Files", "*.*")))
 
     # Using try in case user types in unknown file or closes without choosing a file.
     try:
@@ -78,8 +92,35 @@ def savefile():
     donothing()
 
 
-def printfile():
-    """Print file's data to stdout and terminal.
+def check_xyz():
+    """Check if the input file chosen by user is XYZ format
+    The first word of file must be positive integer.
+    """
+    f = open(filename, "r")
+    line = f.readlines()[0]
+    firstword = line.split(' ')[0]
+    print(firstword)
+    print(type(firstword))
+    print(firstword.isdigit())
+
+    if firstword.isdigit() == True:
+        print("ddd")
+    else:
+        popup_wrong_format()
+        clear_cache()
+
+
+def get_coord():
+    """get coordinate from text file.
+    Molecular coordinates must be in XYZ (cartesian) format
+
+    XZY Format
+    ----------
+    <number of atoms>
+    comment line
+    <element> <X> <Y> <Z>
+    ...
+
     """
 
     # check if input file exist
@@ -87,8 +128,46 @@ def printfile():
         popup_nofile_error()
         return 1
 
-    print("Command: Show file data")
-    print(filedata)
+    # check if input file is xyz format
+    # check_xyz()
+
+    print("Command: Get XYZ coordinates")
+
+    global atom_list
+    global coord_list
+    f = open(filename, "r")
+    # read lines with skips first two lines
+    line = f.readlines()[2:]
+    f.close()
+    atom_raw = []
+
+    for l in line:
+        # read only the 1st column, elements, and pass into array
+        lst = l.split(' ')[0]
+        atom_raw.append(lst)
+    # remove multiple lines after the last element
+    atom_list = atom_raw[0:6]
+    print("Command: Show atom lists")
+    print(atom_list)
+
+    """Read file again for getting XYZ coordinate
+        We have two ways to do this, 
+        1. use >> f.seek(0) <<
+        2. use >> f = open(filename, "r") <<
+    """
+    f = open(filename, "r")
+    # read lines with skips first two lines
+    coord_raw = np.loadtxt(f, skiprows=2, usecols=[1, 2, 3])
+    f.close()
+    # get only coordinates of 1 central atom and 6 ligand atoms
+    coord_list = coord_raw[0:7]
+    print("Command: Show Coordinate lists")
+    print(coord_list)
+
+    """Return both lists of atom_list and coord_list. 
+    To use list, just grab a tuple, and use >> atom_list, coord_list = get_coord() <<
+    """
+    return atom_list, coord_list
 
 
 def prog_help():
@@ -133,7 +212,7 @@ def prog_help():
                  "  <element 3> <X> <Y> <Z>\n" \
                  "  ...\n"
     msg = Message(hp, text=msg_Help_2, width="450")
-    msg.config(font=("Segoe-UI 10 italic"))
+    msg.config(font="Segoe-UI 10 italic")
     msg.pack(anchor=W)
 
     # References
@@ -193,7 +272,13 @@ def popup_open_error():
 def popup_nofile_error():
     """Show error message when opening file twice"""
     print("Error: No input file")
-    showinfo("Error", "No input file. Please load input file by clicking \"Browse file\"")
+    showinfo("Error", "No input file. Click \"Browse file\" to load a new file.")
+
+
+def popup_wrong_format():
+    """Show error message when opening file twice"""
+    print("Error: Not XYZ format")
+    showinfo("Error", "Your input file is not XYZ format.")
 
 
 def popup_author():
@@ -223,87 +308,157 @@ def popup_license():
                         "along with this program. If not, see <https://www.gnu.org/licenses/>.")
 
 
-def donothing():
-    filewin = Toplevel(root)
-    button = Button(filewin, text="Do nothing button")
-    button.pack()
+def dot_product(x, y):
+    return sum([x[i] * y[i] for i in range(len(x))])
 
 
-def get_coord():
-    """get coordinate from text file.
-    Molecular coordinates must be in XYZ (cartesian) format
+def norm(x):
+    return sqrt(dot_product(x, x))
 
-    XZY Format
-    ----------
-    <number of atoms>
-    comment line
-    <element> <X> <Y> <Z>
-    ...
 
+def normalize(x):
+    return [x[i] / norm(x) for i in range(len(x))]
+
+
+def project_onto_plane(x, n):
+    d = dot_product(x, n) / norm(n)
+    p = [d * normalize(n)[i] for i in range(len(n))]
+    return [x[i] - p[i] for i in range(len(x))]
+
+
+def vector_project():
+    return 0
+
+
+def distance_avg(x):
+    """Calculate mean M-X distance
     """
-    print("Command: Get XYZ coordinates")
-    f = open(filename, "r")
-    # read lines with skips first two lines
-    line = f.readlines()[2:]
-    atom_raw = []
+    dist_sum = []
 
-    for l in line:
-        # read only the 1st column, elements, and pass into array
-        lst = l.split(' ')[0]
-        atom_raw.append(lst)
-    # remove multiple lines after the last element
-    atom_list = atom_raw[0:6]
-    print(atom_list)
+    for i in range(1, 7):
+        results_sum = sqrt(pow(x[i][0] - x[0][0], 2) + pow(x[i][1] - x[0][1], 2) + pow(x[i][2] - x[0][2], 2))
+        dist_sum.append(results_sum)
 
-    """Read file again for getting XYZ coordinate
-        We have two ways to do this, 
-        1. use >> f.seek(0) <<
-        2. use >> f = open(filename, "r") <<
+    dist_avg = sum(dist_sum) / 6
+
+    return dist_avg
+
+
+def octa_dist_distance(x):
+    """Calculate 1st octahedral distortion parameter, delta(d).
+    delta(d) = 1/6 * sum[ ((d_i - d)/d)**2 ],
+                where d_i is individual M-X distance,
+                and d is mean M-X distance.
+    Ref: DOI: 10.1107/S0108768103026661   Acta Cryst. (2004). B60, 10-20
     """
-    f = open(filename, "r")
-    # read lines with skips first two lines
-    coord_raw = np.loadtxt(f, skiprows=2, usecols=[1, 2, 3])
-    coord_list = coord_raw[0:6]
-    print(coord_list)
-    """Return both lists of atom_list and coord_list. 
-    To use list, just grab a tuple, and use >> atom_list, coord_list = get_coord() <<
-    """
-    return atom_list, coord_list
+    print("Command: Calculate octahedral distortion parameter: distance")
+    global dist_sum, dist_results
+
+    dist_indi = []
+    dist_results = 0
+
+    for i in range(1, 7):
+        results_indi = sqrt(pow(x[i][0] - x[0][0], 2) + pow(x[i][1] - x[0][1], 2) + pow(x[i][2] - x[0][2], 2))
+        dist_indi.append(results_indi)
+
+    # There are two ways to calculate delta(d) parameter
+    # 1st way
+    for i in range(6):
+        dist_results = (dist_indi[i] - distance_avg(x)) / (distance_avg(x) * 6) + dist_results
+    # 2nd way
+    # dist_results = sum([(dist_indi[i] - distance_avg(x)) / (distance_avg(x) * 6) for i in range(6)])
+
+    return dist_results
 
 
-def vector_proj():
-    print("Vector projection")
+def octa_dist_angle():
+    print("Command: Calculate octahedral distortion parameter: angle")
+    return 0
+
+
+def octa_dist_dihedral():
+    print("Command: Calculate octahedral distortion parameter: dihedral")
+    return 0
 
 
 def calc_para():
     """Calculate octahedral distortion parameters
     For more details in algorithm and method used in this function,
     please refer to following articles.
-    1. J. K. McCusker, A. L. Rheingold, D. N. Hendrickson, Inorg. Chem. 1996, 35, 2100.
-    2. M. Marchivie, P. Guionneau, J. F. Letard, D. Chasseau, Acta Crystal-logr. Sect. B Struct. Sci. 2005, 61, 25.
+    1. J. K. McCusker, A. L. Rheingold, D. N. Hendrickson,
+       Inorg. Chem. 1996, 35, 2100.
+    2. M. Marchivie, P. Guionneau, J. F. Letard, D. Chasseau,
+       Acta Crystal-logr. Sect. B Struct. Sci. 2005, 61, 25.
     """
-
     # check if input file exist
     if filename == "":
         popup_nofile_error()
         return 1
+    else:
+        print("Command: Calculate octahedral distortion parameters")
+        print("         - Distance parameter")
+        print("         - Angle parameter")
+        print("         - Dehedral parameter")
 
-    get_coord()
+    print(octa_dist_distance(coord_list))
+    textBox_distance.insert(INSERT, dist_results)
+
+    textBox_angle.insert(INSERT, "0.0")
+
+    textBox_dihedral.insert(INSERT, "0.0")
+
+    # p = project_onto_plane([3, 4, 5], [1, 2, 3])
+    # print(p)
+
+
+def plot_vector():
+    """Displaying 3D structure of octahedral complex using Matplotlib
+    """
+    # check if input file exist
+    if filename == "":
+        popup_nofile_error()
+        return 1
+    print("Command: Show scatter plot of coordinate")
+
+    # Create point for metal center
+    x_M = coord_list[0, 0]
+    y_M = coord_list[0, 1]
+    z_M = coord_list[0, 2]
+    # Create points for ligand atoms
+    x_lg = coord_list[1:, 0]
+    y_lg = coord_list[1:, 1]
+    z_lg = coord_list[1:, 2]
+
+    # Plot and configuration
+    fig = plt.figure()
+    ax = fig.add_subplot(111, projection='3d')
+
+    ax.scatter(x_M, y_M, z_M, color='yellow', marker='o', s=200, linewidths=2, edgecolors='blue')
+    ax.scatter(x_lg, y_lg, z_lg, color='red', marker='o', s=100, linewidths=2, edgecolors='blue')
+    #ax.plot(x_M, y_M, z_M, color='b')
+
+    ax.set_xlabel(r'X', fontsize=15)
+    ax.set_ylabel(r'Y', fontsize=15)
+    ax.set_zlabel(r'Z', fontsize=15)
+    ax.set_title('Octahedral structure')
+    ax.grid(True)
+    # plt.axis('equal')
+    plt.show()
 
 
 """Start program. Create program UI using TkInter.
-root is used as a master frame.
+master/root is used as a master frame.
 ==============================================================
 """
 # Create main window
 master = Tk()
-# Set Tahoma as a global font with size of 10.
+# Set font and text size as default setting.
 FONT = "Segoe-UI 10"
 master.option_add("*Font", FONT)
 # Set program title
 master.title("Octahedral Distortion Analysis")
 # width x height + x_offset + y_offset
-master.geometry("450x600+200+100")
+master.geometry("450x680+200+100")
 # Uncomment command below, user can resize window freely
 # master.resizable(0,0)
 # Cause pressing <Esc> to close the window.
@@ -319,6 +474,7 @@ Create menu bar
     |- New          << clear_cache
     |- Open         << openfile
     |- Save as ..   << savefile
+    |- Exit         << root.quit
     Help 
     |- Help         << prog_help
     |- Author       << popup_author    
@@ -366,10 +522,6 @@ msg_2 = Label(root, wraplength=400, justify=CENTER,
                    "two octahedral structures.", )
 msg_2.pack(pady=5)
 
-# global variables
-filename = ""
-filedata = ""
-
 # button to browse input file
 btn_openfile = Button(root, command=openfile, text="Browse file", )
 btn_openfile.pack(anchor=W, pady="5")
@@ -380,34 +532,49 @@ para_1.pack(anchor="w", pady=5)
 
 # text box for showing cartesian coordinates
 textBox_1 = Text(root, height="12", wrap="word")
-textBox_1.pack(side="top", fill="both")
+textBox_1.pack(side="top", fill="both", pady="5")
 
-# button to print file data
-btn_printfile = Button(root, text='Print file data', command=printfile)
-btn_printfile.pack(pady="10")
+# button to get coordinate from file
+btn_getcoor = Button(root, text='Get coordinate', command=get_coord)
+btn_getcoor.pack(pady="0")
 
 # button to run
 btn_run = Button(root, command=calc_para, text="Run")
-btn_run.pack()
+btn_run.pack(pady="10")
 
-# text box to show result
-para_1 = Label(root, text="Parameter 1")
+# -----------------------------------------------------------
+
+# Octahedral distortion parameters
+para_0 = Label(root, text="Octahedral distortion parameters")
+para_0.config(font="Sogoe 10 bold")
+para_0.pack(anchor="w")
+
+# Distance
+para_1 = Label(root, text="D (in Ångström)")
 para_1.pack(anchor="w", pady=5)
-# text box for displaying result
-textBox_2 = Text(root, height="1", width=1, wrap="word")
-textBox_2.pack(side="top", fill="both")
-para_2 = Label(root, text="Parameter 2")
+textBox_distance = Text(root, height="1", width=1, wrap="word")
+textBox_distance.pack(side="top", fill="both")
+
+# Angle
+para_2 = Label(root, text="Σ (in degree)")
 para_2.pack(anchor="w", pady=5)
-# text box for displaying result
-textBox_3 = Text(root, height="1", width=1, wrap="word")
-textBox_3.pack(side="top", fill="both")
+textBox_angle = Text(root, height="1", width=1, wrap="word")
+textBox_angle.pack(side="top", fill="both")
+
+# Dehedral
+para_3 = Label(root, text="Θ (in degree)")
+para_3.pack(anchor="w", pady=5)
+textBox_dihedral = Text(root, height="1", width=1, wrap="word")
+textBox_dihedral.pack(side="top", fill="both")
+
+# button to plot vector
+btn_plot = Button(root, command=plot_vector, text="Plot")
+btn_plot.pack(pady="10")
 
 # activate the window
 root.mainloop()
 
-"""if __name__ == '__main__':
-    main()
-
+"""
 with open(filename, 'r') as f:
         configfile.insert(INSERT, f.read())
 """
