b9d24abd76fa69087220e48926ae0bfca37c66dd
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2019-05-21 14:39:42 +0700

    updated example python files for running the tests

--- example-py/example_4.py
+++ example-py/example_4.py
@@ -1,13 +1,32 @@
-from octadist import coord, draw
+###################################################
+# Example 4 for running the test on OctaDist PyPI #
+###################################################
 
-#############
-# Example 4 #
-#############
+from octadist import coord, calc
 
-# Open and read input file
 file = r"../example-input/Multiple-metals.xyz"
 
-# Graphical display for octahedral complex
-full_atom, full_coor = coord.get_coord(file)
-draw.all_atom(full_atom, full_coor)
+atom_full, coor_full = coord.extract_file(file)
 
+# If complex contains metal center more than one, you can specify the index metal
+# whose octahedral structure will be computed.
+# For example, this complex contains three metal atoms: Fe, Ru, and Rd.
+# I add "2" as a second argument for choosing Ru as metal of interest.
+
+atom_octa, coor_octa = coord.extract_octa(atom_full, coor_full, 2)
+
+zeta, delta, sigma, theta = calc.calc_all(coor_octa)
+
+print("\nAll computed parameters")
+print("-----------------------")
+print("Zeta  =", zeta)
+print("Delta =", delta)
+print("Sigma =", sigma)
+print("Theta =", theta)
+
+# All computed parameters
+# -----------------------
+# Zeta  = 0.001616439510534251
+# Delta = 3.5425830613072754e-08
+# Sigma = 1.26579367508117
+# Theta = 4.177042495798965
