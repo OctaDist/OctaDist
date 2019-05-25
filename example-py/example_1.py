c2d80addefff5002868b520bc077b1a912735ff8
RANGSIMAN KETKAEW <rangsiman1993@gmail.com>
2019-05-25 17:52:29 +0700

    Edited example files.

--- example-py/example_1.py
+++ example-py/example_1.py
@@ -2,22 +2,25 @@
 # Example 1 for running the test on OctaDist PyPI #
 ###################################################
 
-from octadist import calc
+import octadist as oc
 
 # The first atom must be metal center atom of octahedral structure.
 # If not, please see example_2.py for how to handle this issue.
 
 atom = ['Fe', 'O', 'O', 'N', 'N', 'N', 'N']
 
-coor = [[2.298354000, 5.161785000, 7.971898000],  # <- Metal atom
-        [1.885657000, 4.804777000, 6.183726000],
-        [1.747515000, 6.960963000, 7.932784000],
-        [4.094380000, 5.807257000, 7.588689000],
-        [0.539005000, 4.482809000, 8.460004000],
-        [2.812425000, 3.266553000, 8.131637000],
-        [2.886404000, 5.392925000, 9.848966000]]
+coord = [[2.298354000, 5.161785000, 7.971898000],  # <- Metal atom
+         [1.885657000, 4.804777000, 6.183726000],
+         [1.747515000, 6.960963000, 7.932784000],
+         [4.094380000, 5.807257000, 7.588689000],
+         [0.539005000, 4.482809000, 8.460004000],
+         [2.812425000, 3.266553000, 8.131637000],
+         [2.886404000, 5.392925000, 9.848966000]]
 
-zeta, delta, sigma, theta = calc.calc_all(coor)
+zeta = oc.calc_zeta(coord)             # Zeta
+delta = oc.calc_delta(coord)           # Delta
+sigma = oc.calc_sigma(coord)           # Sigma
+theta = oc.calc_theta(coord)           # Theta
 
 print("\nAll computed parameters")
 print("-----------------------")
--- example-py/example_5.py
+++ example-py/example_5.py
@@ -2,17 +2,11 @@
 # Example 5 for running the test on OctaDist PyPI #
 ###################################################
 
-from octadist import coord, draw
+import octadist as oc
 
 file = r"../example-input/Multiple-metals.xyz"
 
+atom_full, coord_full = oc.coord.extract_file(file)
+
 # Graphical display for octahedral complex
-
-atom_full, coor_full = coord.extract_file(file)
-draw.all_atom(atom_full, coor_full)
-
-# Display and automatically save image as .png file with user-specified name
-
-draw.all_atom(atom_full, coor_full, "complex_octadist")
-
-# Output image, complex_octadist.png, is stored at ../images directory
+oc.draw.all_atom(atom_full, coord_full)
