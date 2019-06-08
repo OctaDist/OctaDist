3afd82e37db5a2295b11ba53aeab642bfc162a27
Nutt <rangsiman1993@gmail.com>
2019-06-08 16:50:51 +0700

    Edited __revision__ to __release__.

--- octadist_gui/Run.py
+++ octadist_gui/Run.py
@@ -1,11 +1,11 @@
 import octadist_gui.main
 
 version = octadist_gui.__version__
-revision = octadist_gui.__release__
+release = octadist_gui.__release__
 
 if __name__ == '__main__':
     print(f"\nProgram Started >>>")
-    print(f"... OctaDist {version} {revision} ...")
+    print(f"... OctaDist {version} {release} ...")
 
     octadist_gui.main.main()
 
