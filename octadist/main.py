d51f300f7e80b2251a6e27be35e07b45e5119536
Nutt <rangsiman1993@gmail.com>
2019-06-12 10:57:54 +0700

    Updated docstrings.

--- octadist/main.py
+++ octadist/main.py
@@ -580,11 +580,11 @@ class OctaDist:
 
         See Also
         --------
-        octadist.src.calc.CalDistortion.calc_d_mean
-        octadist.src.calc.CalDistortion.calc_zeta
-        octadist.src.calc.CalDistortion.calc_delta
-        octadist.src.calc.CalDistortion.calc_sigma
-        octadist.src.calc.CalDistortion.calc_theta
+        src.calc.CalcDistortion.calc_d_mean
+        src.calc.CalcDistortion.calc_zeta
+        src.calc.CalcDistortion.calc_delta
+        src.calc.CalcDistortion.calc_sigma
+        src.calc.CalcDistortion.calc_theta
 
         """
         # if not self.has_metal:
