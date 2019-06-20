b229f8fefdf672aaae709b7203b7532f0fba5e0d
Nutt <rangsiman1993@gmail.com>
2019-06-20 20:51:05 +0700

    Renamed run_gui --> run in __main__ module.

--- octadist/__main__.py
+++ octadist/__main__.py
@@ -24,7 +24,7 @@ version = octadist.__version__
 release = octadist.__release__
 
 
-def run_gui():
+def run():
     print(f"\nProgram Starts >>>")
     print(f"... OctaDist {version} {release} ...")
 
@@ -43,4 +43,4 @@ if __name__ == '__main__':
     octadist        : OctaDist GUI (default)
     octadist_cli    : OctaDist CLI  
     """
-    run_gui()
+    run()
