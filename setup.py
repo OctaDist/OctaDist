7c40e676232d63f4e782a556da7955f6584723c9
Rangsiman Ketkaew <rangsiman1993@gmail.com>
2019-08-24 14:28:52 +0700

    Add copyright message

--- setup.py
+++ setup.py
@@ -1,5 +1,21 @@
 #!/usr/bin/env python
 
+# OctaDist  Copyright (C) 2019  Rangsiman Ketkaew et al.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+# ++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
 # Metadata file for distribution of OctaDist software on PyPI
 # https://packaging.python.org/guides/distributing-packages-using-setuptools/
 
