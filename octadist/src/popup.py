ac3d0d0f3d5596eae763a16c4118393355e0f5bc
Rangsiman Ketkaew <rangsiman1993@gmail.com>
2020-12-18 17:36:53 +0100

    Edit messages

--- octadist/src/popup.py
+++ octadist/src/popup.py
@@ -18,28 +18,23 @@ from tkinter.messagebox import showinfo, showerror, showwarning
 
 
 def err_no_file():
+    """Show this error when no input files uploaded/opened.
     """
-    Show this error when having no any input files is loaded into program.
-
-    """
-    showerror("Error", "No input file. At least one input file must be loaded.")
+    showerror("Error", "No input file. You need to upload at least one input file.")
 
 
 def err_invalid_ftype():
-    """
-    Show this error popup when file type is not supported by the program.
-
+    """Show this error popup when file type is not supported by the program.
     """
     showerror(
-        "Error",
-        f"Invalid input file format.\n\n"
-        "The complex must have at least 1 metal atom and 6 ligand atoms.",
+        "Error", f"Invalid input file format.\n\n"
+        "Supported formats are *.xyz, *.out, *.log.\n"
+        "Also note that molecule must contain one metal atom and six ligand atoms.",
     )
 
 
 def err_no_coord(i):
-    """
-    Show this error popup when the program cannot read the atomic coordinates of complex
+    """Show this error popup when the program cannot read the atomic coordinates of complex
     inside the file or cannot extract the coordinates from the complex.
 
     This will happen only if the input has no the proper format of atomic coordinates.
@@ -48,18 +43,15 @@ def err_no_coord(i):
     ----------
     i : int
         Number of file.
-
     """
     showerror(
-        "Error",
-        f"File no. {i} has no atomic coordinates.\n\n"
-        "Please make sure the input file format is correct.",
+        "Error", f"File no. {i} Atomic coordinates not found.\n\n"
+        "Please make sure that the input file is correct.",
     )
 
 
 def err_less_ligands(i):
-    """
-    Show this error popup when the complex has ligand atoms less that six.
+    """Show this error popup when the complex has ligand atoms less that six atoms.
 
     Parameters
     ----------
@@ -68,169 +60,127 @@ def err_less_ligands(i):
 
     """
     showerror(
-        "Error",
-        f"File no. {i} has ligand atoms less than six atoms.\n\n"
-        "Please check if some ligand atoms are so far away from metal atom,"
-        "or check if the metal-ligand bond cutoff is set properly.",
+        "Error", f"File no. {i} has ligand atoms less than six atoms.\n\n"
+        "Please check if some ligand atoms are too far away from metal atom,"
+        "if so, please check the metal-ligand bond cutoff and set this parameter properly.",
     )
 
 
 def err_no_metal():
-    """
-    Show this error popup when the complex has no transition metal atom.
-
+    """Show this error popup when the complex has no transition metal atom.
     """
     showerror(
-        "Error",
-        "Cannot proceed calculation. Your current structure has no transition metal.\n\n"
-        "The complex must have at least one transition metal.",
+        "Error", "Cannot proceed calculation. Transition metal not found in your current structure.\n\n"
+        "Molecule must contain at least one transition metal.",
     )
 
 
 def err_no_calc():
-    """
-    Show this error popup when the user requests function that the results are required,
+    """Show this error popup when the user requests function that the results are required,
     but the results have not been computed yet.
-
     """
     showerror(
-        "Error",
-        'No results. Click "Compute" to calculate octahedral distortion parameters.',
+        "Error", 'No results. Click "Compute" to calculate distortion parameters of the molecule.',
     )
 
 
 def err_only_2_files():
-    """
-    Show this error popup when having not uploaded two complexes
+    """Show this error popup when having not uploaded two complexes
     for using the RMSD function.
-
     """
     showerror("Error", "You must load only two complexes for computing RMSD.")
 
 
 def err_not_equal_atom():
-    """
-    Show this error popup when the total number of atoms of two complexes are not equal.
-
+    """Show this error popup when the total number of atoms of two complexes are not equal.
     """
     showerror(
-        "Error", "The number of atoms in structure 1 and structure 2 are not the same."
+        "Error", "The number of atoms in structure 1 and structure 2 are not equal."
     )
 
 
 def err_atom_not_match(line):
-    """
-    show this error popup when atomic symbol of two similar complexes does not match.
+    """show this error popup when atomic symbol of two similar complexes does not match.
 
     Parameters
     ----------
     line : int
         The line number that atomic symbol does not match.
-
     """
     showerror("Error", f"Atomic symbol not match at line {line}.")
 
 
 def err_many_files():
+    """Show this error popup when user has loaded too many files.
     """
-    Show this error popup when user has loaded too many files.
-
-    """
-    showerror("Error", "You must load only one input file.")
+    showerror("Error", "Too many input files. You can load only one input file.")
 
 
 def err_wrong_format():
-    """
-    Show this error popup when user has loaded the file that is not supported by OctaDist.
-
+    """Show this error popup when user has loaded the file that is not supported by OctaDist.
     """
     showerror(
-        "Error",
-        "Could not read file.\n\n"
-        "Input file format is not supported by the current version of OctaDist.",
+        "Error", "Could not read file. Input file format is not supported yet by the current version of OctaDist.\n\n"
+        "However, please contact OctaDist developer if you have any further questions or need help."
     )
 
 
 def err_no_editor():
+    """Show this error popup if text editor path is empty.
     """
-    Show this error popup if text editor path is empty.
-
-    """
-    showerror(
-        "Error",
-        "No text editor.\n\n"
-        "Text editor path is empty. Go to setting and browse a new text editor.",
-    )
+    showerror("Error", "Text editor path is empty. Go to program setting and browse a new text editor.")
 
 
 def err_cannot_update():
-    """
-    Show this error popup when the program cannot detect the operating system of
-    the machine that user is using.
-
+    """Show this error popup when the program cannot detect the operating system that the user is using.
     """
     showerror(
-        "Error",
-        "Cannot download an installer of a new version.\n\n"
-        "Please contact OctaDist development team for further help.",
+        "Error", "Cannot download an installer of a new version.\n\n"
+        "Please contact OctaDist developer for further help."
     )
 
 
 def info_save_results(file):
-    """
-    Show info popup:
-
-    show this info popup when output file has been saved successfully.
+    """show this info popup when an output file has been saved successfully.
 
     Parameters
     ----------
     file : str
         Absolute or full path of saved output file.
-
     """
     showinfo("Info", f"Data has been saved to {file}")
 
 
 def info_new_update():
+    """Show this info popup when new version is available for update.
     """
-    Show this info popup when new version is available for update.
-
-    """
-    showinfo("Info", "New updates available!")
+    showinfo("Info", "New release available! Please upgrade your OctaDist. :)")
 
 
 def info_using_dev():
-    """
-    Show this info popup if user is using a development build version.
-
+    """Show this info popup if user is using a development build version.
     """
     showinfo("Info", "You are using a development build version!")
 
 
 def info_no_update():
-    """
-    Show this info popup if program is the latest version.
-
+    """Show this info popup if program is the latest version.
     """
     showinfo("Info", "You already have the latest version.")
 
 
 def warn_no_metal(i):
-    """
-    Show this warning popup if no transition metal was found.
+    """Show this warning popup if no transition metal was found.
 
     Parameters
     ----------
     i : int
         Number of file.
-
     """
-    showwarning("Warning", f"File no. {i} has no transition metal in your complex.")
+    showwarning("Warning", f"File no. {i} transition metal not found in your molecule.")
 
 
 def warn_not_octa():
+    """Show this warning popup if the complex is non-octahedral structure.
     """
-    Show this warning popup if the complex is non-octahedral structure.
-
-    """
-    showwarning("Warning", "Non-octahedral complex detected.")
+    showwarning("Warning", "Non-octahedral structure detected!")
